31e11adcdb5ccfaad0c88fbb546db1c7fd1d24e1
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2024-03-30 23:12:40 +0530

    DistinictSubsequences.java

---
+++ DistinictSubsequences.java
@@ -0,0 +1,57 @@
+class Solution {
+      int prime = (int) (Math.pow(10, 9) + 7);
+    private int helper(int i,int j,String s,String t,int[][]dp){
+       
+       if(j==0){
+         return 1;
+       }
+
+       if(i==0){
+        return 0;
+       }
+
+       if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s.charAt(i-1)==t.charAt(j-1)){
+            dp[i][j]=helper(i-1,j-1,s,t,dp)+helper(i-1,j,s,t,dp);
+            return dp[i][j];
+        }else{
+            dp[i][j]=helper(i-1,j,s,t,dp);
+            return dp[i][j];
+        }
+    }
+    public int numDistinct(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int[][]dp =new int[n+1][m+1];
+        // for(int i=0;i<=n;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+
+        // return helper(n,m,s,t,dp);
+
+      
+
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+
+        for(int j=1;j<=m;j++){
+            dp[0][j]=0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+             if(s.charAt(i-1)==t.charAt(j-1)){
+               dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%prime;   
+           }else{
+               dp[i][j]=(dp[i-1][j])%prime; 
+              
+        }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
